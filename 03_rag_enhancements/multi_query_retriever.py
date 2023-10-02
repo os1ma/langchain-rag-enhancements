3b4f2e133d68226a897c5bb68d90a6168982bfca
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-10-02 18:11:24 +0900

    Add MultiQueryRetriever sample

---
+++ 03_rag_enhancements/multi_query_retriever.py
@@ -0,0 +1,28 @@
+import langchain
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.vectorstores import FAISS
+
+langchain.verbose = True
+
+load_dotenv()
+
+# FAISSに保存されたベクトルを読み込む
+embeddings = OpenAIEmbeddings()
+db = FAISS.load_local("./tmp/faiss", embeddings)
+base_retriever = db.as_retriever()
+
+# MultiQueryRetrieverを準備
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+retriever = MultiQueryRetriever.from_llm(retriever=base_retriever, llm=chat)
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=chat, chain_type="stuff", retriever=retriever
+)
+
+query = "LangChainとは"
+result = qa_chain.run(query)
+print(result)
